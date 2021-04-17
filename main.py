4a81fac6fec9b196f7c7387e581efbbb751ff7fd
DaminiTIjare <daminitijare19@outlook.com>
2021-04-17 14:22:03 +0530

    few changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask,request,render_template
 import pickle
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return render_template("login.html")
 database={'damini':'123','james':'aac','radhika':'asdsf'}
 
-@app.route('/form_login',methods=['POST','GET'])
+@application.route('/form_login', methods=['POST', 'GET'])
 def login():
     name1=request.form['username']
     pwd=request.form['password']
@@ -21,4 +21,4 @@ def login():
 	         return render_template('home.html',name=name1)
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
